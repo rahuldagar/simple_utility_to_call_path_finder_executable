fea6782ec79c5cc35a0c3539aa21a51972612be8
rahuldagar <rahuldagar@outlook.com>
2014-01-01 11:31:19 +0530

    Path finder
    Path finder code implemented in C++ called from ASP.Net web portal. Code
    was reused.

---
+++ Code/Default.aspx.vb
@@ -0,0 +1,31 @@
+﻿Imports System.Diagnostics
+
+Partial Class _Default
+    Inherits System.Web.UI.Page
+    Declare Sub Sleep Lib "kernel32" Alias "Sleep" _
+   (ByVal dwMilliseconds As Long)
+
+    Sub sleep()
+
+    End Sub
+    Protected Sub Button1_Click(ByVal sender As Object, ByVal e As System.EventArgs) Handles Button1.Click
+        Dim i, j As Integer
+
+        Shell("f:\INS.exe" & " " & TextBox1.Text)
+        Sleep()
+
+
+    End Sub
+
+    Protected Sub TextBox1_TextChanged(ByVal sender As Object, ByVal e As System.EventArgs) Handles TextBox1.TextChanged
+
+    End Sub
+
+    Protected Sub Page_PreLoad(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.PreLoad
+
+    End Sub
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+
+    End Sub
+End Class
